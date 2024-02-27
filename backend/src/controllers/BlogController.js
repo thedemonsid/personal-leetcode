88562934cefcb460c4dbbf768e75dceb717967d2
thedemonsid <siddheshshrirame@gmail.com>
2024-02-28 01:03:50 +0530

    Add BlogController and blogRoutes and blog model

---
+++ backend/src/controllers/BlogController.js
@@ -0,0 +1,53 @@
+import { Blog } from "../models/blog.model.js";
+//logic to get All Blogs from DB
+export async function getAllBlogsController(req, res, next) {
+  try {
+    const blogs = await Blog.find({});
+    if (!blogs.length) {
+      return res.status(200).json({
+        success: true,
+        message: "No Blogs On DB",
+      });
+    }
+    res.status(200).json({
+      success: true,
+      message: "successful acquisition of blogs",
+      blogs,
+      length: blogs.length,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Error while getting Blogs",
+      error,
+    });
+  }
+}
+export async function createBlogController(req, res, next) {
+  try {
+    const { title, content, image } = req.body;
+    if (!title || !content || !image) {
+      return res.status(404).json({
+        success: false,
+        message: "Please send all Content to create a blog",
+        error,
+      });
+    }
+    const blog = new Blog({ title, content, image });
+    await blog.save();
+    res.status(200).json({
+      success: true,
+      message: "Successfully created the blog",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Error while creating Blog",
+      error,
+    });
+  }
+}
+export async function getBlogController() {}
+export async function updateBlogController() {}
+export async function deleteBlogController() {}
---
+++ backend/src/routes/blogRoutes.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { createBlogController, getAllBlogsController } from "../controllers/BlogController.js";
+const router = express.Router();
+//Get All Blogs
+router.get("/all-blogs", getAllBlogsController);
+//Create A Blog
+router.put("/create-blog", createBlogController);
+// //Get Blog
+// router.get("/get-blog/:id", getBlogController);
+// //Update A Blog
+// router.put("/update-blog/:id", updateBlogController);
+// //Delete A Blog
+// router.delete("/delete-blog/:id", deleteBlogController);
+
+export default router;
