da48c14af00fd8fd4a948b6a5ffb42f9e0aed628
Vdcds <dumbdevmate@gmail.com>
2024-03-01 16:14:39 +0530

    Temporary commits for auth change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,13 @@
 import { useState } from 'react'
-
 const App = () => {
   return (
-    <div className='text-2xl bg-cyan-400'><h1>App</h1></div>
+    <div className='w-full h-2 bg-red-200 '>
+      <div className="navright">
+        <button className='loginbutton'> Login</button>
+        <button className='getin'> Get In </button>
+        
+      </div>
+    </div>
   )
 }
 
